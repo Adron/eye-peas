e02c443b25b4ccc13a6df7d33ab321c366193bea
Adron Hall <adron@thrashingcode.com>
2024-05-22 17:33:33 -0700

    The refactored code.

--- main.go
+++ main.go
@@ -10,23 +10,9 @@ func main() {
 }
 
 func IpsBetween(start, end string) int {
-	startIP := net.ParseIP(start).To4()
-	endIP := net.ParseIP(end).To4()
-
-	if startIP == nil || endIP == nil {
-		panic(fmt.Sprintf("Invalid IPv4 address: start=%s, end=%s", start, end))
-	}
-
-	startNum := ipToInt(startIP)
-	endNum := ipToInt(endIP)
-
-	return int(endNum - startNum)
+	return int(ipToInt(net.ParseIP(end).To4()) - ipToInt(net.ParseIP(start).To4()))
 }
 
 func ipToInt(ip net.IP) uint32 {
-	result := uint32(ip[0]) << 24
-	result |= uint32(ip[1]) << 16
-	result |= uint32(ip[2]) << 8
-	result |= uint32(ip[3])
-	return result
+	return uint32(ip[0])<<24 | uint32(ip[1])<<16 | uint32(ip[2])<<8 | uint32(ip[3])
 }
